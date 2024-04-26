6d39f0aa62cfb6895f430636e01ab21ffba5fb0c
Lblzr <itsgdblazar69@gmail.com>
2024-04-26 14:35:44 -0700

    STABLE: fixed LONG floating terminal

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *termfloat[] = { "st", "-c", "flt", "-g", "85x120+600+450" };
+static const char *termfloat[] = { "st", "-c", "flt", "-g", "80x24+677+65" };
 
 #include "shiftview.c"
 static const Key keys[] = {
