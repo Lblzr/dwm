98ea38ebff1f2d804f37f07ce012016dba4eea63
Lblzr <itsgdblazar69@gmail.com>
2024-05-05 19:32:57 -0700

    WIP, STABLE: Lot of patch changes, flip flopping, small changes, etc., etc. Work on setting up colour schemes in .Xresources, and work on getting xrdb running properly in .xinitrc.

--- config.def.h
+++ config.def.h
@@ -10,16 +10,18 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font Mono:style=Bold:size=11:antialias=true:autohint=true", "JetBrainsMonoNL NF:style=Regular:size=11:antialias=true:autohint=true" };
 static const char dmenufont[]       = "RobotoMono Nerd Font:style=Bold:size=11:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
+
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -34,24 +36,30 @@ static const Rule rules[] = {
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "flt",      NULL,	  NULL,	      0,	    1,		 -1 },
 	// Minecraft 
-    	{ "minecraft-launcher", NULL, NULL,   1 << 2,	    0,		 -1 }, //  Tag 3 (Modify if needed)
+    	{ "Minecraft Launcher", NULL, NULL,   1 << 2,	    0,		 -1 }, //  Tag 3
+	{ "Modrinth-app", NULL,	  NULL,	      1 << 2,	    0,		 -1 }, //  Tag 3
 
    	// Steam 
-    	{ "Steam",    NULL, 	  NULL,       1 << 2, 	    0, 		 -1 }, //  Tag 3 (Modify if needed)
+    	{ "steam",    NULL, 	  NULL,       1 << 2, 	    0, 		 -1 }, //  Tag 3
+	{ NULL,	      NULL,	  "Steam",    1 << 2,	    0,		 -1 }, //  Tag 3
+	{ "steamwebhelper", NULL, NULL,	      1 << 2,	    0,		 -1 }, //  Tag 3
 
    	// Vivaldi & Vivaldi Stable
-  	{ "Vivaldi",  NULL,	  NULL,	      1 << 1,	    0,           -1 }, //  Tag 2 (Modify if needed)
+  	{ "Vivaldi",  NULL,	  NULL,	      1 << 1,	    0,           -1 }, //  Tag 2
 	{ "Vivaldi-stable", NULL, NULL,	      1 << 1,	    0,		 -1 }, //  Tag 2
 
    	// Vesktop (Discord)
-   	{ "vesktop",  NULL, 	  NULL,	      1 << 8,	    0,		 -1 }, //  Tag 9 
+   	{ "vesktop",  NULL, 	  NULL,	      1 << 8,	    0,		 -1 }, //  Tag 9
 
   	// Spotify 
-   	{ "spotify",  NULL,	  NULL,	      1 << 7, 	    0,		 -1 }, //  Tag 8
+   	{ "Spotify",  NULL,	  NULL,	      1 << 7, 	    0,		 -1 }, //  Tag 8
 
    	// VLC & players
   	{ "vlc",      NULL,	  NULL,	      1 << 4,	    0,		 -1 }, //  Tag 5
 	{ NULL,	      NULL,	  "player",   1 << 4,	    0,		 -1 }, //  Tag 5
+	
+	{ "jetbrains-toolbox", NULL, NULL,    1 << 0,	    1,		 -1 }, //  Tag 1
+	{ "jetbrains-idea", NULL, NULL,	      1 << 0,	    0,		 -1 }, //  Tag 1
 };
 
 /* layout(s) */
@@ -85,7 +93,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 
 static const char *termcmd[]  = { "st", NULL };
 static const char *termfloat[] = { "st", "-c", "flt", "-g", "80x24+677+65", NULL };
@@ -127,6 +135,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY,			XK_F5,	   xrdb,	   {.v = NULL} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
