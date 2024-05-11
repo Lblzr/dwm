915c35681e9297ba13cc7bf98fdcdd7ad7d8d0b5
Lblzr <itsgdblazar69@gmail.com>
2024-05-10 23:14:20 -0700

    STABLE: Fixed that god forsaken THICK ass border finally and removed smartborder, overcomplicated version of noborder that encroaches upon floatrules.

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 32;        /* gaps between windows */
 static const unsigned int snap      = 4;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
